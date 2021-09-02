cdf1d88e4200adb362a2c32bc31948abc968ebe3
최형원 <cgd0502@gmail.com>
2021-09-02 19:53:26 +0900

    20210902 Project Create

---
+++ BootStart/src/test/java/com/start/myapp/BootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.start.myapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
